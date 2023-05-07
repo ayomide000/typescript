6ba95d40c7226a76813f3bbc92338f3d0ffd2972
Akande Gbolahan <akandegbolahanayomide@gmail.com>
2023-05-07 16:22:36 +0100

    first commit

---
+++ Lesson One/build/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let username = 'Ayo';
+console.log(username);
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
+/*
+TypeScript is a strongly typed while javaScript is loosely typed programming language
+TypeScript is a statistically typed language, this means types are checked at compiple time while JS is a dynamically typed language, this means types are checked at run time
+
+*/ 
---
+++ Lesson One/src/main.ts
@@ -0,0 +1,16 @@
+let username = 'Ayo'
+console.log(username);
+
+let a: number = 12
+let b: number = 6 
+let c: number = 2
+
+console.log(a/b);
+
+console.log(c * b);
+
+/*
+TypeScript is a strongly typed while javaScript is loosely typed programming language
+TypeScript is a statistically typed language, this means types are checked at compiple time while JS is a dynamically typed language, this means types are checked at run time
+
+*/
